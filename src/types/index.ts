03d099fc6cbc570113f93a934c5400a0d15d61c2
zhouhaotian730 <zhouhaotian730@pingan.com.cn>
2020-11-12 17:55:12 +0800

    10-1，10-2 取消

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,6 +24,7 @@ export interface AxiosRequestConfig {
   timeOut?: number
   transformRequest?: AxiosTransformer | AxiosTransformer[]
   transformResponse?: AxiosTransformer | AxiosTransformer[]
+  cancelToken?: CancelToken
 
   [propName: string]: any
 }
@@ -95,4 +96,28 @@ export interface RejectedFn {
 
 export interface AxiosTransformer {
   (data: any, headers?: any): any
+}
+
+export interface CancelToken {
+  promise: Promise<string>
+  reason?: string
+}
+
+export interface Canceler {
+  (message?: string): void
+}
+
+export interface CancelExecutor {
+  (cancel: Canceler): void
+}
+
+export interface CancelTokenSource {
+  token: CancelToken
+  cancel: Canceler
+}
+
+export interface CancelTokenStatic {
+  new (executor: CancelExecutor): CancelToken
+
+  source(): CancelTokenSource
 }
