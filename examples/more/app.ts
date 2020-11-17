c599d2df3aad2511b925eccadc01896fa96367c8
zhouhaotian730 <zhouhaotian730@pingan.com.cn>
2020-11-17 14:24:57 +0800

    11-4 csrf demo

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -1,17 +1,26 @@
 import axios from '../../src/index'
 
-document.cookie = 'a=b'
+// document.cookie = 'a=b'
+//
+// axios.get('/more/get').then(res => {
+//   console.log('success', res)
+// })
+//
+// axios.post('http://127.0.0.1:8088/more/server2',{
+//   a:1
+// },{
+//   withCredentials: true
+// }).then(res => {
+//   console.log('withCredentials success', res)
+// })
 
-axios.get('/more/get').then(res => {
-  console.log('success', res)
+const instance = axios.creat({
+  xsrfCookieName: 'XSFR-COOKIE-D',
+  xsrfHeaderName: 'X-XSFR-COOKIE-D'
 })
 
-axios.post('http://127.0.0.1:8088/more/server2',{
-  a:1
-},{
-  withCredentials: true
-}).then(res => {
-  console.log('withCredentials success', res)
+instance.get('/more/get').then(res => {
+  console.log(res)
 })
 
 
--- examples/server.js
+++ examples/server.js
@@ -27,7 +27,11 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(webpackHotMiddleware(compiler))
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname, {
+  setHeaders (res) {
+    res.cookie('XSFR-COOKIE-D', '123asd')
+  }
+}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
